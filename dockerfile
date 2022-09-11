4b31e1957f24b4b8004e902b9b8874125f8ffffd
Letícia Marques <iqme@pop-os.localdomain>
2022-09-11 14:58:53 -0300

    Added dockerfile and docker-compose

---
+++ dockerfile
@@ -0,0 +1,7 @@
+from node:14
+
+workdir /rentalx
+
+COPY /rentalx .
+
+expose 8080
--- rentalx/src/routes/categories.routes.ts
+++ rentalx/src/routes/categories.routes.ts
@@ -1,9 +1,10 @@
 import {Router} from "express";
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { PostgresCategoryRepository } from "../repositories/PostgresCategoryRepository";
 import { CreateCategoriesService } from "../services/CreateCategoriesService";
 
 const categoriesRoutes = Router();
-const categoryRepository = new PostgresCategoryRepository();
+const categoryRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req, res) => {
 	const {name, description} =  req.body;
