faa11edf32dc15f72d9ce7977f7baf8ac1df0142
Letícia Marques <iqme@pop-os.localdomain>
2022-09-13 17:35:34 -0300

    Documentation

--- rentalx/src/server.ts
+++ rentalx/src/server.ts
@@ -1,9 +1,11 @@
 import express, { Router } from "express";
 import { router } from "./routes";
-import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationsRoutes } from "./routes/specifications.routes";
+import swaggerUi  from "swagger-ui-express";
+import swaggerFile  from './swagger.json';
 
 const app = express();
 app.use(express.json());
 app.use(router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 app.listen(8080, () => console.log("Pops"))
